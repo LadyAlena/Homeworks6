700efaec72616d86c850cd8687bc5b834ca5d44a
LadyAlena <107322107+LadyAlena@users.noreply.github.com>
2022-08-02 11:19:18 +0300

    Update Source.cpp

--- 01/Source.cpp
+++ 01/Source.cpp
@@ -34,7 +34,7 @@ int main(int argc, char* argv) {
 			std::cout << a << " / " << b << " = " << Divide(a, b) << std::endl;
 			
 		}
-		catch(std::exception ex) {
+		catch(std::logic_error ex) {
 			std::cout << ex.what() << std::endl;
 		}
 
